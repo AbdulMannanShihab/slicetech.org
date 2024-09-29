aa7f5a038a9eaba5e19952f2d6ba2762e8935fcf
mannanshihab <amannanshihab@gmail.com>
2024-09-29 12:52:40 +0600

    install-Filament-admin-panel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Mannan Shihab',
+            'email' => 'info@slicetech.org',
+            'password' => bcrypt('Mannan07'),
         ]);
     }
 }
