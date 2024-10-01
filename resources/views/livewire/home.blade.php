588a1e03fe612b3f82d986fdf64702082867b91b
mannanshihab <amannanshihab@gmail.com>
2024-10-01 23:46:11 +0600

    update

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -49,7 +49,7 @@
     </div>
     <!-- End Card Blog -->
     <div class="flex justify-end pb-5 md:pt-0 mr-10">
-        <a wire:navigate class="py-2.5 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="{{ route('blogs') }}"> 
+        <a wire:navigate class="py-2.5 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/"> 
             Read More
             <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
         </a>
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,4 @@ use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class);
-Route::get('/blogs', Blogs::class)->name('blogs');
 Route::get('/blog-details/{slug}', BlogDetails::class)->name('blog-details');
