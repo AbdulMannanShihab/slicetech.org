0974f7462cc3ad9ec0fa2f3aa0ea88fc943c5910
mannanshihab <amannanshihab@gmail.com>
2024-10-07 16:25:30 +0600

    update

--- app/Livewire/BlogDetails.php
+++ app/Livewire/BlogDetails.php
@@ -3,23 +3,26 @@
 namespace App\Livewire;
 
 use App\Models\Post;
+use GuzzleHttp\Promise\Create;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 #[Title('Blog Details')]
 class BlogDetails extends Component
 {
-    public $user_id;
+    public $auther;
     public $title;
     public $image;
     public $description;
+    public $create_at;
 
     public function mount($slug)
     {
         $post = Post::where('slug', $slug)->first();
-        $this->user_id = $post->user->name;
+        $this->auther = $post->user->name;
         $this->title = $post->title;
         $this->image = $post->image;
         $this->description = $post->description;
+        $this->create_at = $post->created_at->diffForHumans();
     }
     public function render()
     {
