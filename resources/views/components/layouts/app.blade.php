877bde807ac3cee5e64e23b5bf73dad86d8ec7a3
mannanshihab <amannanshihab@gmail.com>
2024-12-22 11:17:48 +0600

    add:seo-without-img

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,13 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        {!! seo() !!}
         <meta name="description" 
               content="Dive into Laravel, PHP, Livewire, and Filament-PHP with expert insights and tutorials. 
               Learn how to build dynamic web applications efficiently using modern frameworks and tools. 
               Stay updated with the latest tips and best practices in PHP development.">
         <meta name="author" content="Mannan Shihab">
         <link rel="icon" href="{{ asset('asset/images/SliceTechFavicon.png') }}">
+        
+        @if(request()->is('/'))
+            <title> {{ config('app.name', 'Slice-Tech') }}</title>
+        @else
+            {!! seo() !!}
+        @endif
+        
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
