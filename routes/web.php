8fe478e84858765ac227f37c17fda47644a008ef
mannanshihab <amannanshihab@gmail.com>
2025-04-11 21:40:13 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use App\Livewire\SingleCategoryPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class);
-Route::get('/blog-details/{slug}', BlogDetails::class)->name('blog-details');
+Route::get('/{slug}', BlogDetails::class)->name('blog-details');
 
 Route::get('/category/{slug}', SingleCategoryPage::class)->name('category');
